efc8c8e74b1ca66cfb41593cc4967b16b25f94e4
Wolfyxon <wolfyxon@gmail.com>
2024-07-17 18:13:05 +0200

    Init background animation

--- src/scenes/game/game.lua
+++ src/scenes/game/game.lua
@@ -1,3 +1,39 @@
 local scene = require("lib.scene"):new()
 
+local circles = 10
+local circleRot = 0
+
+function scene:draw(screen)
+    local sW, sH = love.graphics.getDimensions(screen)
+    local ratio = sW / sH
+
+    if not screen or screen == "left" then
+        
+    end
+
+    if not screen or screen == "bottom" then
+        --love.graphics.setBackgroundColor(0.6, 0.5, 0)
+
+
+        for i = 1, circles do
+            love.graphics.push()
+            love.graphics.translate(sW / 2, sH / 2)
+            local dir = (-1) ^ i
+            
+            love.graphics.rotate(circleRot * dir)
+
+            local c = i / circles
+            love.graphics.setColor(0.6 * c, 0.5 * c, 0, 1)
+            love.graphics.circle("fill", 0 , 0, (circles - i) * 100 * ratio, 6)
+
+            love.graphics.pop()
+        end
+        
+    end 
+end
+
+function scene:update(delta)
+    circleRot = circleRot + delta * 0.2
+end
+
 return scene
