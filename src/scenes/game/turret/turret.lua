ef09ad93fe64819b35a0223f23290f7f5e771285
Wolfyxon <wolfyxon@gmail.com>
2024-07-18 15:25:09 +0200

    Init firing animation

--- src/scenes/game/turret/turret.lua
+++ src/scenes/game/turret/turret.lua
@@ -12,6 +12,8 @@ function Turret:new(o)
 
     o.targetRotation = 0
     o.rotationSpeed = 5
+    o.fireCooldown = 0.2
+    o.lastFireTime = 0
 
     o.base = Sprite:new({}, "scenes/game/turret/img/base.png")
     o.cannon = Sprite:new({}, "scenes/game/turret/img/cannon.png")
@@ -33,10 +35,24 @@ function Turret:update(delta)
     if love.mouse.isCursorSupported() then
         local x, y = utils.getMousePos()
         self.targetRotation = self:rotationTo(x, y)
+    end
 
+    if love.mouse.isDown(1) then
+        self:fire()
     end
 
     self.rotation = utils.lerpAngle(self.rotation, self.targetRotation, self.rotationSpeed * delta)
+    self.cannon.x = utils.lerp(self.cannon.x, 0.2, 5 * delta)
+end
+
+function Turret:fire()
+    local now = love.timer.getTime()
+    if now < self.lastFireTime + self.fireCooldown then 
+        return 
+    end
+    self.lastFireTime = now
+
+    self.cannon.x = 0.15
 end
 
 return Turret
