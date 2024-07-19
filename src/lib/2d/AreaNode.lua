ef0f297558c42daaabf941b1a82aaee65e766f07
Wolfyxon <wolfyxon@gmail.com>
2024-07-19 15:59:44 +0200

    Added getRectCorners()

--- src/lib/2d/AreaNode.lua
+++ src/lib/2d/AreaNode.lua
@@ -1,3 +1,4 @@
+local utils = require("lib.utils")
 local Node2D = require("lib.2d.node2d")
 local AreaNode = Node2D:new()
 
@@ -45,6 +46,17 @@ end
 
 function AreaNode:getSize()
     return self.width, self.height
+end
+
+function AreaNode:getRectCorners()
+    local cx = self.x + self.width / 2
+    local cy = self.y + self.height / 2
+    
+    return {
+        utils.math.rotatePoint(self.x + self.width, self.y, cx, cy, self.rotation),
+        utils.math.rotatePoint(self.x + self.width, self.y + self.height, cx, cy, self.rotation),
+        utils.math.rotatePoint(self.x, self.y + self.height, cx, cy, self.rotation)
+    }
 end
 
 return AreaNode
