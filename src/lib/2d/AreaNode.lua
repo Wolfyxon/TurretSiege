8de862cefc55aa0bf1b92f39cf2cd32fed1a2274
Wolfyxon <wolfyxon@gmail.com>
2024-07-19 13:55:41 +0200

    Added bodyType property

--- src/lib/2d/AreaNode.lua
+++ src/lib/2d/AreaNode.lua
@@ -1,12 +1,14 @@
 local Node2D = require("lib.2d.node2d")
 local AreaNode = Node2D:new()
 
+AreaNode.bodyTypes = {"rect", "circle"}
 
 function AreaNode:new(o, path)
     o = Node2D.new(self, o)
     setmetatable(o, self)
     self.__index = self
 
+    o.bodyType = "rect"
     o.height = nil
     o.width = nil
 
