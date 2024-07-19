a7dc8050f785b4d4f03b1c551cf1a6651d1f6732
Wolfyxon <wolfyxon@gmail.com>
2024-07-19 13:56:03 +0200

    Renamed body* -> hitbox*

--- src/lib/2d/AreaNode.lua
+++ src/lib/2d/AreaNode.lua
@@ -1,14 +1,14 @@
 local Node2D = require("lib.2d.node2d")
 local AreaNode = Node2D:new()
 
-AreaNode.bodyTypes = {"rect", "circle"}
+AreaNode.hitboxTypes = {"rect", "circle"}
 
 function AreaNode:new(o, path)
     o = Node2D.new(self, o)
     setmetatable(o, self)
     self.__index = self
 
-    o.bodyType = "rect"
+    o.hitboxType = "rect"
     o.height = nil
     o.width = nil
 
