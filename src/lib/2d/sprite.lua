9bd3b0223f90992b0522e3aec842876d9ce6610f
Wolfyxon <wolfyxon@gmail.com>
2024-07-22 14:06:30 +0200

    Added shadows

--- src/lib/2d/sprite.lua
+++ src/lib/2d/sprite.lua
@@ -1,4 +1,5 @@
 local gameData = require("data")
+local utils    = require("lib.utils")
 
 local AreaNode = require("lib.2d.AreaNode")
 
@@ -10,6 +11,15 @@ Sprite.texture = nil
 
 ---@type number
 Sprite.textureRotation = 0
+
+---@type boolean
+Sprite.enableShadow = true
+
+---@type number
+Sprite.shadowOffset = 15
+
+---@type number
+Sprite.shadowOpaticy = 0.4
 
 function Sprite:new(o, path)
     o = AreaNode.new(self, o)
@@ -36,6 +46,22 @@ function Sprite:draw()
     local tW, tH = self:getTextureSize()
 
     love.graphics.rotate(math.rad(self.textureRotation))
+
+    if self.enableShadow then
+        local r, g, b, a = love.graphics.getColor()
+
+        local ox, oy = utils.math.rotateDirection(self.shadowOffset, self.shadowOffset, -self:getGlobalRotation())
+        
+        love.graphics.translate(ox, oy)
+
+        love.graphics.setColor(0, 0, 0, self.shadowOpaticy)
+        love.graphics.draw(self.texture, -(tW / 2), -(tH / 2))
+
+        love.graphics.setColor(r, g, b, a)
+        love.graphics.translate(-ox, -oy)
+    end
+
+
     love.graphics.draw(self.texture, -(tW / 2), -(tH / 2))
 end
 
