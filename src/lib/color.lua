66f273c2a4b1f29b555c7f0eb47ab13d75e85a9e
Wolfyxon <wolfyxon@gmail.com>
2024-07-24 21:03:29 +0200

    Init Color class

---
+++ src/lib/color.lua
@@ -0,0 +1,18 @@
+local Color = {}
+
+---@param r number
+---@param g number
+---@param b number
+---@param a? number
+function Color:new(r, g, b, a)
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+
+    o.r = r
+    o.g = g
+    o.b = b
+    o.a = a or 1
+end
+
+return Color
