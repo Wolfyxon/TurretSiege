bb34eeea08aaeda366ec56714c900041bbb0c519
Wolfyxon <wolfyxon@gmail.com>
2024-07-25 19:46:13 +0200

    Added cannon ball

---
+++ src/scenes/game/projectiles/cannonBall/cannonBall.lua
@@ -0,0 +1,20 @@
+local Projectile = require("scenes.game.projectile")
+
+---@class CannonBallProjectile: Projectile
+local CannonBallProjectile = Projectile:new()
+CannonBallProjectile:_appendClass("AxeProjectile")
+
+CannonBallProjectile.speed = 0.4
+CannonBallProjectile.rotationSpeed = 1000
+CannonBallProjectile.damage = 5
+
+local scale = 0.25
+
+function CannonBallProjectile:ready()
+    self.scaleX = scale
+    self.scaleY = scale
+
+    self:loadTextureFromFile("scenes/game/projectiles/cannonBall/cannonBall.png")
+end
+
+return CannonBallProjectile
