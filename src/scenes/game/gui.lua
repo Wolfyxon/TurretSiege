bb05579b066bc04086688be2ae9679042a1430b3
Wolfyxon <wolfyxon@gmail.com>
2024-07-28 15:55:55 +0200

    Added health bar

--- src/scenes/game/gui.lua
+++ src/scenes/game/gui.lua
@@ -1,10 +1,47 @@
 local Node2D = require("src.lib.2d.Node2d")
+local ListContainer = require("lib.2d.gui.container.ListContainer")
+local Label = require("src.lib.2d.gui.Label")
+local ProgressBar = require("lib.2d.gui.ProgressBar")
 
 ---@class GameGui: Node2D
 local GameGui = Node2D:new()
 
+GameGui.healthDisplay = nil ---@type Node2D
+
 function GameGui:ready()
     self.screen = "left"
+
+    --== Health display ==--
+    local healthDisplay = ListContainer:new()
+    healthDisplay.spacing = 0.02
+    healthDisplay.x = 0.5
+    healthDisplay.y = 0.02
+
+    local hpLbl = Label:new()
+    hpLbl.textScale = 0.3
+    hpLbl:setText("Turret health")
+    healthDisplay:addChild(hpLbl)
+
+    local hpBar = ProgressBar:new()
+    hpBar.width = 0.5
+    hpBar.height = 0.04
+
+    self.hpBar = hpBar
+    healthDisplay:addChild(hpBar)
+    self.healthDisplay = healthDisplay
+
+
+    self:addChild(healthDisplay)
+    healthDisplay:arrangeChildren()
+end
+
+function GameGui:update()
+    local turret = self.scene.turret ---@type Turret
+
+    if turret then
+        self.hpBar.value = turret.hp
+        self.hpBar.max = turret.maxHp
+    end
 end
 
 return GameGui
