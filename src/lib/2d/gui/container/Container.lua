66006d388b826203fa06dfd7817381d77b80f425
Wolfyxon <wolfyxon@gmail.com>
2024-07-28 13:57:10 +0200

    Init Container

---
+++ src/lib/2d/gui/container/Container.lua
@@ -0,0 +1,24 @@
+local GuiNode = require("lib.2d.gui.GuiNode")
+
+---@class Container: GuiNode
+local Container = GuiNode:new()
+
+function Container:new(o)
+    o = GuiNode.new(self, o)
+    setmetatable(o, self)
+    self.__index = self
+
+    o:onEvent("ready", function()
+        self:arrangeChildren()
+    end)
+
+    o:onEvent("nodeListUpdated", function()
+        self:arrangeChildren()
+    end)
+
+    return o
+end
+
+function Container:arrangeChildren() end
+
+return Container
