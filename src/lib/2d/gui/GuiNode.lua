0270a4109c5182780958424dea6c19cbb921c6ed
Wolfyxon <wolfyxon@gmail.com>
2024-07-28 13:27:57 +0200

    Added background and border

--- src/lib/2d/gui/GuiNode.lua
+++ src/lib/2d/gui/GuiNode.lua
@@ -1,6 +1,50 @@
 local AreaNode = require("lib.2d.AreaNode")
+local Color    = require("lib.Color")
 
 ---@class GuiNode: AreaNode
 local GuiNode = AreaNode:new()
+
+GuiNode.backgroundColor = nil ---@type Color
+GuiNode.borderColor = nil     ---@type Color
+GuiNode.borderSize = 0        ---@type number
+GuiNode.borderRadius = 0.1    ---@type number
+
+function GuiNode:new(o)
+    o = AreaNode.new(self, o)
+    setmetatable(o, self)
+    self.__index = self
+
+    o.backgroundColor = Color:new(0.5, 0.5, 0.5)
+    o.backgroundColor = Color:new(1, 1, 1)
+
+    return o
+end
+
+function GuiNode:draw()
+    local ox = -(self.width / 2)
+    local oy = -(self.height / 2)
+
+    love.graphics.setColor(self.backgroundColor:getRGBA())
+    love.graphics.rectangle(
+        "fill",
+        self.x + ox,
+        self.y + oy,
+        self.width,
+        self.height,
+        self.borderRadius / self.width,
+        self.borderRadius / self.height
+    )
+
+    love.graphics.setColor(self.borderColor:getRGBA())
+    love.graphics.rectangle(
+        "line", 
+        self.x + ox,
+        self.y + oy,
+        self.width,
+        self.height,
+        self.borderRadius / self.width,
+        self.borderRadius / self.height
+    )
+end
 
 return GuiNode
