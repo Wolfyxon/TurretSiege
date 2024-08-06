f605235c7f7d8edd021c9a4fa429a897ebe9911e
Wolfyxon <wolfyxon@gmail.com>
2024-08-06 11:29:45 +0200

    Separated Node from Node2D

--- src/lib/2d/Node2d.lua
+++ src/lib/2d/Node2d.lua
@@ -1,156 +1,32 @@
 local utils = require("lib.utils")
 local Color = require("lib.Color")
+local Node = require("lib.Node")
 
----@class Node2D
-local Node2D = {
-    eventHandlers = {},        ---@type {string: function[]}
-    scene = nil,               ---@type Scene
-    main = nil,                ---@type Main
-    parent = nil,              ---@type Node2D
-    children = {},             ---@type Node2D[]
-    visible = true,            ---@type boolean
-    screen = "inherit",        ---@type "all" | "inherit" | "left" | "bottom" -- NOTE: the top screen is called 'left'
-    rotation = 0,              ---@type number
-    scaleX = 1,                ---@type number
-    scaleY = 1,                ---@type number
-    isReady = false,           ---@type boolean
-    classList = {"Node2D"},    ---@type string[]
-    x = 0,                     ---@type number
-    y = 0,                     ---@type number
-    color = Color:new(1, 1, 1) ---@type Color
-}
+---@class Node2D: Node
+local Node2D = Node:new()
+
+Node2D.visible = true             ---@type boolean
+Node2D.screen = "inherit"         ---@type "all" | "inherit" | "left" | "bottom" -- NOTE: the top screen is called 'left'
+Node2D.rotation = 0               ---@type number
+Node2D.scaleX = 1                 ---@type number
+Node2D.scaleY = 1                 ---@type number
+Node2D.x = 0                      ---@type number
+Node2D.y = 0                      ---@type number
+Node2D.color = Color:new(1, 1, 1) ---@type Color
+
+Node2D:_appendClass("Node2D")
 
 function Node2D:new(o)
-    o = o or {}
+    o = Node.new(self, o)
     setmetatable(o, self)
     self.__index = self
-
-    o.eventHandlers = utils.table.copy(self.eventHandlers)
+    
     o.color = Color:new(1, 1, 1)
-    o.children = {}
 
     return o
 end
 
 --== Dynamic methods ==--
-
----@param ... string
-function Node2D:_registerEvent(...)
-    for i, event in ipairs({...}) do
-        self.eventHandlers[event] = {}
-    end
-end
-
----@param name string
-function Node2D:emitEvent(name, ...)
-    local handlers = self.eventHandlers[name]
-    assert(handlers, "Unknown event '" .. name .. "'")
-
-    for i, v in ipairs(handlers) do
-        v(...)
-    end
-end
-
----@param name string
----@param handler function
-function Node2D:onEvent(name, handler)
-    local handlers = self.eventHandlers[name]
-    assert(handlers, "Unknown event '" .. name .. "'")
-
-    table.insert(handlers, handler)
-end
-
----@return integer|nil
-function Node2D:getIndex()
-    if not self.parent then return end
-
-    for i, v in ipairs(self.parent.children) do
-        if v == self then
-            return i
-        end
-    end
-end
-
----@return string
-function Node2D:getClass()
-    return self.classList[#self.classList]
-end
-
----@param className string
-function Node2D:_appendClass(className)
-    self.classList = utils.table.copy(self.classList)
-    table.insert(self.classList, className)
-end
-
----@param class string
----@param exact? boolean
----@return boolean
-function Node2D:isA(class, exact)
-    if exact then
-        return self:getClass() == class
-    end
-
-    return utils.table.find(self.classList, class) ~= nil
-end
-
----@return Node2D[]
-function Node2D:getAncestors()
-    local res = {}
-
-    local current = self.parent
-
-    while current do
-        table.insert(res, current)
-        current = current.parent
-    end
-
-    return res
-end
-
----@return Node2D[]
-function Node2D:getDescendants()
-    local res = {}
-
-    for i, v in ipairs(self.children) do
-        table.insert(res, v)
-        
-        for ii, vv in ipairs(v:getDescendants()) do
-            table.insert(res, vv)
-        end
-    end
-
-    return res
-end
-
----@param class string
----@param exact? boolean
----@return Node2D[]
-function Node2D:getChildrenOfClass(class, exact)
-    local res = {}
-
-    for i, v in ipairs(self.children) do
-        if v:isA(class, exact) then
-            table.insert(res, v)
-        end
-    end
-
-    return res
-end
-
----@param class string
----@param exact? boolean
----@return Node2D[]
-function Node2D:getDescendantsOfClass(class, exact)
-    local res = {}
-
-    for i, v in ipairs(self:getDescendants()) do
-        if v:isA(class, exact) then
-            table.insert(res, v)
-        end
-    end
-
-    return res
-end
 
 ---@return number
 function Node2D:getGlobalRotation()
@@ -228,54 +104,6 @@ function Node2D:getGlobalScale()
     return sx, sy
 end
 
-function Node2D:orphanize()
-    if not self.parent then return end
-    self.parent:disownChild(self)
-end
-
----@param node Node2D
-function Node2D:addChild(node)
-    node:orphanize()
-    node.parent = self
-    node.main = node.main or self.main
-    
-    node.scene = self.scene
-    if self:isA("Scene") then
-        node.scene = self
-    end
-
-    table.insert(self.children, node)
-
-    node:emitEvent("added", self)
-    node:added(self)
-
-    if not node.isReady then
-        node:emitEvent("ready")
-        node:ready()
-    end
-
-    self:emitEvent("nodeAdded", node)
-    self:emitEvent("nodeListUpdated")
-end
-
----@param node Node2D
-function Node2D:disownChild(node)
-    table.remove(self.children, node:getIndex())
-    node.parent = nil
-    node:emitEvent("removed")
-    node:removed(self)
-    self:emitEvent("nodeRemoved", node)
-    self:emitEvent("nodeListUpdated")
-end
-
-function Node2D:destroy()
-    for i, v in ipairs(self:getDescendants()) do
-        v:orphanize()
-    end
-
-    self:orphanize()
-end
-
 ---@return "all" | "left" | "bottom"
 function Node2D:getTargetScreen()
     if self.screen ~= "inherit" then
@@ -322,15 +150,6 @@ function Node2D:drawRequest(screen, data)
     love.graphics.pop()
 end
 
----@param delta number
-function Node2D:updateRequest(delta)
-    self:update(delta)
-
-    for i, v in ipairs(self.children) do
-        v:updateRequest(delta)
-    end
-end
-
 ---@param x number
 ---@param y number
 function Node2D:rotationTo(x, y)
@@ -358,38 +177,5 @@ end
 
 ---@param screen nil|"left"|"bottom"
 function Node2D:draw(screen) end
-
----@param delta number
-function Node2D:update(delta) end
-
----@param newParent Node2D
-function Node2D:added(newParent) end
-
----@param previousParent Node2D
-function Node2D:removed(previousParent) end
-
-function Node2D:ready() end
-
---== Static methods ==--
-
----@param instance table
----@return boolean
-function Node2D.isNode2D(instance)
-    if type(instance) ~= "table" then
-        return false
-    end
-
-    local classList = instance.classList
-
-    if not classList or type(classList) ~= "table" then
-        return false
-    end
-
-    return utils.table.find(classList, "Node2D") ~= nil
-end
-
---== Post logic ==--
-
-Node2D:_registerEvent("ready", "added", "removed", "nodeAdded", "nodeRemoved", "nodeListUpdated")
 
 return Node2D
