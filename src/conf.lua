61b22b2064cb2dc4a5535b17ed0e3b31609a8dc3
Wolfyxon <wolfyxon@gmail.com>
2024-08-16 13:22:50 +0200

    Exported some util functions into core lua libraries

---
+++ src/conf.lua
@@ -0,0 +1,27 @@
+-- This script is for exporting util functions
+-- into Lua globals so they can be used more easily.
+-- Example instead of `utils.string.startsWith("hi", "h")`: `"hi":startsWith("h")`
+-- (I would use a for loop but Intelisense is too stupid)
+-- NOTE: conf.lua runs before main.lua
+
+local utils = require("lib.utils")
+
+--== Table ==--
+table.keys = utils.table.keys
+table.find = utils.table.find
+table.random = utils.table.random
+table.tostring = utils.table.tostring
+table.copy = utils.table.copy
+table.erase = utils.table.erase
+
+--== String ==--
+string.startsWith = utils.string.startsWith
+string.endsWith = utils.string.startsWith
+string.split = utils.string.split
+string.replace = utils.string.replace
+
+--== Math ==--
+math.randomf = utils.math.randomf
+math.lerp = utils.math.lerp
+math.lerpAngle = utils.math.lerpAngle
+math.rotationTo = utils.math.rotationTo
