755128910cd2cab447374019d1f73e93ce850df2
Wolfyxon <wolfyxon@gmail.com>
2024-08-19 08:03:25 +0200

    Init effect display

--- src/scenes/game/gui.lua
+++ src/scenes/game/gui.lua
@@ -1,12 +1,15 @@
 local Node2D = require("lib.2d.Node2d")
 local ListContainer = require("lib.2d.gui.container.ListContainer")
 local Label = require("lib.2d.gui.Label")
+local Color = require("lib.Color")
 local ProgressBar = require("lib.2d.gui.ProgressBar")
 
 ---@class GameGui: Node2D
 local GameGui = Node2D:new()
 
 GameGui.healthDisplay = nil ---@type Node2D
+GameGui.hasEffect = false ---@type boolean
+GameGui.effectBar = nil ---@type ProgressBar
 
 function GameGui:ready()
     self.screen = "left"
@@ -31,18 +34,54 @@ function GameGui:ready()
     healthDisplay:addChild(hpBar)
     self.healthDisplay = healthDisplay
 
+    local effectBar = ProgressBar:new()
+    effectBar.textDisplayStyle = "none"
+    effectBar.barPositioning = "center"
+    effectBar:setText("Effect name")
+    effectBar.barColor = Color:new(0.5, 1, 0.5)
+    effectBar.width = 0.5
+    effectBar.height = 0.02
+    effectBar.scaleY = 0
+    
+    healthDisplay:addChild(effectBar)
+    self.effectBar = effectBar
 
     self:addChild(healthDisplay)
     healthDisplay:arrangeChildren()
 end
 
-function GameGui:update()
+function GameGui:update(delta)
     local turret = self:getScene().turret ---@type Turret
 
     if turret then
         self.hpBar.value = turret.hp
         self.hpBar.max = turret.maxHp
     end
+
+    local effectBarSpeed = 0.5
+    
+    if self.hasEffect then
+        self.effectBar.scaleY = math.lerp(self.effectBar.scaleY, 1, effectBarSpeed * delta)
+    else
+        self.effectBar.scaleY = math.lerp(self.effectBar.scaleY, 0, effectBarSpeed * delta)
+    end
+end
+
+---@param name string
+---@param duration number
+---@param color Color
+function GameGui:setEffect(name, duration, color)
+    local bar = self.effectBar
+
+    bar:setText(name)
+    bar.value = bar.max
+    self.hasEffect = true
+
+    local t = self:createTween()
+              :addKeyframe(bar, {
+                value = 0
+              }, duration)
+    t:play()
 end
 
 return GameGui
