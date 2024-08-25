0e3df72ddbdbfc5813dc449da765228d86f461cd
Wolfyxon <wolfyxon@gmail.com>
2024-08-25 19:39:16 +0200

    Added title

--- src/scenes/menu/menu.lua
+++ src/scenes/menu/menu.lua
@@ -1,11 +1,39 @@
+local Color = require("lib.Color")
 local Button = require("lib.2d.gui.Button")
+local Label = require("lib.2d.gui.Label")
 local ListContainer = require("lib.2d.gui.container.ListContainer")
 local Scene = require("lib.Scene")
+local data = require("data")
 
 ---@class MenuScene: Scene
 local MenuScene = Scene:new()
 
 function MenuScene:load()
+    --== Title ==--
+
+    local titleContainer = ListContainer:new()
+    titleContainer.x = 0.5
+    titleContainer.y = 0.3
+
+    local title = Label:new()
+    title:setText("Turret Siege")
+    title:setFontSize(64)
+    titleContainer:addChild(title)
+
+    local version = Label:new()
+    version.textAlign = "right"
+    version:setText("v. " .. data.version)
+    version:setFontSize(14)
+    titleContainer:addChild(version)
+
+    local author = Label:new()
+    author:setText("by Wolfyxon")
+    titleContainer:addChild(author)
+    
+    titleContainer:arrangeChildren()
+
+    --== Buttons ==--
+
     local w = 0.5
     local h = 0.05
     local fontSize = 24
@@ -36,7 +64,10 @@ function MenuScene:load()
     buttonContainer:addChild(btnQuit)
 
     buttonContainer:arrangeChildren()
+
+    self:addChild(titleContainer)
     self:addChild(buttonContainer)
+    
 end
 
 return MenuScene
