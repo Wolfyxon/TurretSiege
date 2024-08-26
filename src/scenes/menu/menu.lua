b3050bd4ef0932204d00036c44e741ac6a49551a
Wolfyxon <wolfyxon@gmail.com>
2024-08-27 00:17:19 +0200

    Added more gears

--- src/scenes/menu/menu.lua
+++ src/scenes/menu/menu.lua
@@ -39,8 +39,10 @@ function MenuScene:load()
     --== Gears ==--
     
     self:addGear(0, 0, 1, 1)
+    self:addGear(0.05, 0, 0.5, 1)
     self:addGear(0.2, 1, 0.7, -1)
     self:addGear(0.7, 0.1, 0.5, 1)
+    self:addGear(0.5, 0.5, 0.4, 1)
     self:addGear(1, 1, 1.2, -1)
     
     --== Title ==--
