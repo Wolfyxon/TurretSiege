f6b1c5b1b324e2713d6e358fd3b560f6ee5e994f
Wolfyxon <wolfyxon@gmail.com>
2024-10-04 23:00:52 +0200

    Sprite testing

--- src/scenes/debug.lua
+++ src/scenes/debug.lua
@@ -1,4 +1,5 @@
 local Scene = require("lib.2d.Scene")
+local Sprite = require("lib.2d.Sprite")
 local utils = require("lib.utils")
 local data = require("data")
 
@@ -6,12 +7,20 @@ local data = require("data")
 local DebugScene = Scene:new()
 DebugScene:_appendClass("DebugScene")
 
+DebugScene.sprite = nil ---@type Sprite
+
 local console = require("lib.Console"):new()
 
 function DebugScene:load()
     console:print("Debug scene")
     console:print("Game version:", data.version)
     console:print("Platform:", utils.system.getPlatform())
+
+    self.sprite = Sprite:new()
+    self.sprite:loadTextureFromFile("scenes/game/projectiles/bullet/bullet.png")
+    self.x = 0.5
+    self.y = 0.5
+    self:addChild(self.sprite)
 end
 
 function DebugScene:draw(screen)
