982a088ed6d6d5ded833158785ddb54670c29ade
Wolfyxon <wolfyxon@gmail.com>
2024-10-07 20:26:23 +0200

    Reorganized conf.lua

--- src/conf.lua
+++ src/conf.lua
@@ -1,8 +1,8 @@
--- This script is for exporting util functions
--- into Lua globals so they can be used more easily.
--- Example instead of `utils.string.startsWith("hi", "h")`: `"hi":startsWith("h")`
--- (I would use a for loop but Intelisense is too stupid)
 -- NOTE: conf.lua runs before main.lua
+
+
+------========== Util imports ==============-----
+-- (I would use a for loop but Intelisense is too stupid)
 
 local utils = require("lib.utils")
 
