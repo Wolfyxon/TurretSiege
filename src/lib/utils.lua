09ab5e9e980c6bbf6cb120d79ceef5fd893670f3
Wolfyxon <wolfyxon@gmail.com>
2024-10-10 14:38:56 +0200

    Added math.distanceTo()

--- src/lib/utils.lua
+++ src/lib/utils.lua
@@ -279,6 +279,15 @@ function utils.math.rotationToPosition(x1, y1, angle, distance)
     return x2, y2
 end
 
+---@param x1 number
+---@param y1 number
+---@param x2 number
+---@param y2 number
+---@return number
+function utils.math.distanceTo(x1, y1, x2, y2)
+    return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2 )
+end
+
 ---@param x number
 ---@param y number
 ---@param deg number
