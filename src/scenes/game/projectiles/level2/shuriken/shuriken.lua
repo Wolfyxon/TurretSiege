4ab5666e116d8fc0906916507cfdb2a247e2d422
Wolfyxon <wolfyxon@gmail.com>
2024-10-11 15:36:49 +0200

    Shuriken audio is additional

--- src/scenes/game/projectiles/level2/shuriken/shuriken.lua
+++ src/scenes/game/projectiles/level2/shuriken/shuriken.lua
@@ -1,27 +1,30 @@
 local Projectile = require("scenes.game.Projectile")
+local Audio = require("lib.Audio")
 
 ---@class ShurikenProjectile: Projectile
 local ShurikenProjectile = Projectile:new()
 ShurikenProjectile:_appendClass("ShurikenProjectile")
 
 ShurikenProjectile.rotationSpeed = 800
-ShurikenProjectile.level = 2
+ShurikenProjectile.level = 1
 ShurikenProjectile.comminity = 10.2
 ShurikenProjectile.speed = 0.5
 ShurikenProjectile.damage = 8
 ShurikenProjectile.moveTarget = "forward"
-ShurikenProjectile.damageSoundVolume = 0.3
 
 function ShurikenProjectile:ready()
-    self.damageSound = love.audio.newSource("scenes/game/projectiles/audio/metalHit.ogg", "static")
     self:loadTextureFromFile("scenes/game/projectiles/level2/shuriken/shuriken.png")
     self:setScaleAll(0.08)
     self:initHp(4)
     
     self:rotate(math.random(-100, 100))
 
+    local audio = Audio:new():loadFromFile("scenes/game/projectiles/audio/metalHit.ogg", "static"):setVolume(0.3)
+    self:addChild(audio)
+
     self:onEvent("damaged", function ()
         self:rotate(math.random(-20, 20))
+        audio:play()
     end)
 end
 
