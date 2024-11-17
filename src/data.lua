275b7f9bee4371649d354168e558b76e95ce8a4f
Wolfyxon <wolfyxon@gmail.com>
2024-11-17 20:29:22 +0100

    Env compatibility with unreleased Lover

--- src/data.lua
+++ src/data.lua
@@ -1,5 +1,6 @@
 return {
-    version = os.getenv("LOVER_VERSION") or "0.0.0-unknown",
+    -- TODO: Remove LOVER_VERSION after Lover 1.4 (or 1.3.(x > 1)) is released
+    version = os.getenv("LOVER_VERSION") or os.getenv("LOVER_PKG_VERSION") or "0.0.0-unknown",
     windowSizeMultiplier = 3,
     width = 400,
     height = 240,
