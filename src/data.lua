621cfbd33a1cb9abe26924a5d303f2dbcebe358b
Wolfyxon <wolfyxon@gmail.com>
2025-05-02 12:42:50 +0200

    Removed old lover consts

--- src/data.lua
+++ src/data.lua
@@ -1,6 +1,5 @@
 return {
-    -- TODO: Remove LOVER_VERSION after Lover 1.4 (or 1.3.(x > 1)) is released
-    version = os.getenv("LOVER_VERSION") or os.getenv("LOVER_PKG_VERSION") or "0.0.0-unknown",
+    version = os.getenv("LOVER_PKG_VERSION") or "0.0.0-unknown",
     windowSizeMultiplier = 3,
     width = 400,
     height = 240,
