d796ac3538c1eb0c553f6dfc707493cac4a4db67
Wolfyxon <wolfyxon@gmail.com>
2025-05-04 13:57:32 +0200

    Fixed scene loading, now pause menu works correctly

--- src/main.lua
+++ src/main.lua
@@ -95,8 +95,8 @@ function main.loadScene(scene)
     assert(tp == "table", "Not a table. Type: " .. tp)
 
     if currentScene then
-        currentScene:clearChildren()
         currentScene:unload()
+        currentScene:destroy()
     end
 
     scene = scene:new()
