fa3e5d040e8df32b7ce24bab1bc67d1fcccbfc6e
Wolfyxon <wolfyxon@gmail.com>
2025-05-22 09:55:25 +0200

    Decreased windowSieMultiplier

--- src/data.lua
+++ src/data.lua
@@ -1,6 +1,6 @@
 return {
     version = os.getenv("LOVER_PKG_VERSION") or "0.0.0-unknown",
-    windowSizeMultiplier = 3,
+    windowSizeMultiplier = 2.5,
     width = 400,
     height = 240,
     flags = {"scene=menu"}
