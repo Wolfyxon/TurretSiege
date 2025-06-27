8ec28c22d73f8bdd7d4b1f95d0ce81a4bf4853ef
Wolfyxon <wolfyxon@gmail.com>
2025-06-27 20:39:44 +0200

    Setup functions and callbacks

--- src/scenes/game/turret/TurretGun.lua
+++ src/scenes/game/turret/TurretGun.lua
@@ -3,7 +3,7 @@ local Sprite = require("lib.2d.Sprite")
 ---@class TurretGun: Sprite
 local TurretGun = class("TurretGun", Sprite)
 
-TurretGun:_registerEvent("overheat")
+TurretGun:_registerEvent("overheat", "fire")
 
 TurretGun.turret = nil       ---@type Turret
 TurretGun.lastFire = 0       ---@type number
@@ -18,15 +18,40 @@ function TurretGun:canFire()
     return not self.overheat and self:getTime() > self.lastFire + self.cooldown
 end
 
+---@param cooldown number
+---@return self
+function TurretGun:setCooldown(cooldown)
+    self.cooldown = cooldown
+    return self
+end
+
+---@param cooling number
+---@return self
+function TurretGun:setCooling(cooling)
+    self.cooling = cooling
+    return self
+end
+
+---@param cap number
+---@return self
+function TurretGun:setHeatCapacity(cap)
+    self.heatCapacity = cap
+    return self
+end
+
 function TurretGun:update(delta)
     self.heat = math.max(self.heat - self.cooling, 0)
     self.color.r = math.lerp(self.color.r, self.heat / self.heatCapacity, 5 * delta)
+
+    self:updateCallback(delta)
 end
 
 function TurretGun:fire()
     if not self:canFire() then
         return
     end
+
+    self:fireCallback()
 
     self.lastFire = self:getTime()
     self.heat = math.min(self.heat + 1, self.heatCapacity)
@@ -36,5 +61,18 @@ function TurretGun:fire()
         self:emitEvent("overheat")
     end
 end
+
+---@param callback function
+function TurretGun:onFire(callback)
+   self.updateCallback = callback
+end
+
+---@param callback function
+function TurretGun:onUpdate(callback)
+    self.updateCallback = callback
+end
+
+function TurretGun:updateCallback(delta) end
+function TurretGun:fireCallback() end
 
 return TurretGun
