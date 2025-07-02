84985e8ea9636ad738976946bd370a0d5c7f11e3
Wolfyxon <wolfyxon@gmail.com>
2025-07-02 13:08:21 +0200

    Added color shader

---
+++ src/shaders/color.glsl
@@ -0,0 +1,9 @@
+#pragma language glsl1
+
+uniform vec4 color = vec4(1, 1, 1, 1);
+
+vec4 effect(vec4 col, Image texture, vec2 texture_coords, vec2 screen_coords) {
+    vec4 tex_color = Texel(texture, texture_coords);
+    
+    return tex_color * color;
+}
