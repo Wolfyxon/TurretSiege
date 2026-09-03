fc2e5b07a93303b2bed6f6b09d272f44ca08bdc6
Wolfyxon <wolfyxon@gmail.com>
2026-09-03 22:17:39 +0200

    Functional setup for level 1 projectiles

--- src/scenes/game/projectiles/level1/axe/axe.lua
+++ src/scenes/game/projectiles/level1/axe/axe.lua
@@ -1,19 +1,9 @@
 local Projectile = require("scenes.game.Projectile")
 
 ---@class AxeProjectile: Projectile
-local AxeProjectile = class("AxeProjectile", Projectile)
-
-AxeProjectile.speed = 0.3
-AxeProjectile.rotationSpeed = -500
-AxeProjectile.damage = 2
-
-local scale = 0.2
-
-function AxeProjectile:ready()
-    self.scaleX = scale
-    self.scaleY = scale
-
-    self:loadTextureFromFile("scenes/game/projectiles/level1/axe/axe.png")
-end
-
-return AxeProjectile
+return class("AxeProjectile", Projectile):new()
+    :set("speed", 0.3)
+    :set("rotationSpeed", -500)
+    :set("damage", 2)
+    :setScaleAll(0.2)
+    :loadTextureFromFile("scenes/game/projectiles/level1/axe/axe.png")
--- src/scenes/game/projectiles/level1/cannonBall/cannonBall.lua
+++ src/scenes/game/projectiles/level1/cannonBall/cannonBall.lua
@@ -1,19 +1,9 @@
 local Projectile = require("scenes.game.Projectile")
 
 ---@class CannonBallProjectile: Projectile
-local CannonBallProjectile = class("CannonBallProjectile", Projectile)
-
-CannonBallProjectile.speed = 0.4
-CannonBallProjectile.rotationSpeed = 1000
-CannonBallProjectile.damage = 5
-
-local scale = 0.28
-
-function CannonBallProjectile:ready()
-    self.scaleX = scale
-    self.scaleY = scale
-
-    self:loadTextureFromFile("scenes/game/projectiles/level1/cannonBall/cannonBall.png")
-end
-
-return CannonBallProjectile
+return class("CannonBallProjectile", Projectile):new()
+    :set("speed", 0.4)
+    :set("rotationSpeed", 1000)
+    :set("damage", 5)
+    :setScaleAll(0.28)
+    :loadTextureFromFile("scenes/game/projectiles/level1/cannonBall/cannonBall.png")
