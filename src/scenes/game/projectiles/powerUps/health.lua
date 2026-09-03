b98bbcc237b0cfda793203d815e045c17c4c9e07
Wolfyxon <wolfyxon@gmail.com>
2026-09-03 12:47:36 +0200

    Health powerups will appear slightly more often

--- src/scenes/game/projectiles/powerUps/health.lua
+++ src/scenes/game/projectiles/powerUps/health.lua
@@ -4,7 +4,7 @@ local Color = require("lib.Color")
 ---@class HealthPowerUp: PowerUp
 return class("HealthPowerUp", PowerUp):new()
     :setColor(Color:new(0.5, 1, 0.5))
-    :set("commonity", 0.1)
+    :set("commonity", 0.15)
     :set("iconImage", "health")
     :onCollect(function(self)
         self:getScene().turret:heal(25)
